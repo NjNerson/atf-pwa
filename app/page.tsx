72b430997e26e2e3be26b8f75c0e04addbb63ab1
Nj Nerson <nj.nerson@gmail.com>
2025-07-14 22:16:53 +0300

    transform main page.tsx to be with Pagelayout

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import PageLayout from "@/components/layout/PageLayout";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen w-full items-center justify-center bg-gray-100">
+    <PageLayout>
       <div className="relative w-full max-w-4xl">
         <div className="relative z-10 p-8 bg-white bg-opacity-80">
           <h1 className="text-4xl font-bold text-center text-gray-800 mb-6">
@@ -43,49 +44,9 @@ export default function Home() {
             Labore! Sapiente tempora tenetur cum odit repellat placeat. Unde nam
             id officia, voluptatum error tempore ut magnam iure soluta ratione
             illum temporibus velit eos vero officiis amet possimus nostrum sunt
-            aliquam. Quis est molestias libero quod culpa, officiis vero aliquam
-            labore reprehenderit saepe, id expedita! Cum, dolor facilis dicta
-            voluptatum eveniet earum necessitatibus, natus voluptate labore,
-            amet et. Vitae, ullam dolor. Doloremque voluptates dolor impedit
-            quos tempore alias nisi repudiandae repellendus dolorum, quisquam
-            commodi enim sit unde minima. Laborum possimus at error architecto,
-            obcaecati delectus officia maiores temporibus veniam asperiores
-            odio. Doloribus rem sunt suscipit est totam nisi ipsa sapiente,
-            sequi praesentium. Rerum, pariatur laudantium accusamus quidem animi
-            deserunt ut facilis! Molestiae eum laborum similique maxime quidem
-            illo, quo est temporibus! Placeat quia doloremque quaerat corrupti
-            totam, sequi vel culpa. Vel iusto odit nisi laudantium ad sed
-            dolorum, reiciendis deleniti, veritatis corporis officiis aperiam
-            hic mollitia. Numquam cupiditate modi corporis accusantium.
-            Doloribus inventore quaerat recusandae omnis quae eligendi non
-            dignissimos. Excepturi id veritatis odit labore illo eos
-            exercitationem doloribus soluta, asperiores iusto explicabo tempore
-            laborum quisquam atque in necessitatibus, ipsam accusantium.
-            Expedita enim vero dignissimos consectetur. Expedita earum provident
-            dicta. Maiores officia, hic veritatis voluptate, voluptates itaque
-            sint illo cumque nisi aut iste ea quis porro est laboriosam. Nemo,
-            dolorum tenetur. Consequuntur tempore mollitia corrupti aspernatur
-            quo eveniet aliquam hic. Non, labore consectetur iure amet debitis
-            beatae itaque, odio laboriosam architecto praesentium quos fugit
-            autem animi laudantium dolores explicabo, placeat aspernatur!
-            Molestiae voluptate, veritatis reprehenderit ipsam tempora doloribus
-            qui asperiores enim eum omnis quas alias deleniti voluptatum minima
-            voluptates aliquam officiis facere suscipit incidunt earum
-            reiciendis dolore hic! Sequi, unde cum. Consectetur magnam placeat
-            delectus, cumque vero, vitae mollitia debitis quam dicta rerum,
-            praesentium excepturi impedit? Corporis impedit natus delectus quam?
-            In velit eum laborum. Aspernatur vitae fugit velit ea deserunt.
-            Suscipit laboriosam animi cum facere! Culpa dolores, iste
-            consequatur modi reiciendis quaerat, doloremque perferendis aut
-            voluptas expedita corporis earum, error illum rem similique nobis
-            obcaecati. Esse explicabo autem quibusdam ipsum! Dicta quaerat
-            dolorum voluptatibus eligendi quia esse nihil temporibus? Quae quia
-            praesentium ipsam consectetur assumenda sapiente, sint incidunt id
-            explicabo voluptate, necessitatibus quo vero tempora, totam neque
-            exercitationem veniam? Quae.
           </p>
         </div>
       </div>
-    </div>
+    </PageLayout>
   );
 }
