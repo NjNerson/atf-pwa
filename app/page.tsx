1eb63bd78250c917c5587dc52abe2fc775d249bd
Nj Nerson <nj.nerson@gmail.com>
2025-07-14 15:34:36 +0300

    navlinks data

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,87 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+    <div className="flex min-h-screen w-full items-center justify-center bg-gray-100">
       <div className="relative w-full max-w-4xl">
-        <div className="relative z-10 p-8 bg-white bg-opacity-80 rounded-lg shadow-lg">
+        <div className="relative z-10 p-8 bg-white bg-opacity-80">
           <h1 className="text-4xl font-bold text-center text-gray-800 mb-6">
             Bienvenue sur ATF
           </h1>
           <p className="text-lg text-gray-600 text-center">
             Votre application de gestion de tâches et de suivi des activités.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti
+            incidunt inventore aperiam quasi expedita sequi atque non quos autem
+            in aliquid itaque accusamus unde esse repellat odit, necessitatibus
+            mollitia molestias. Voluptatum inventore eos, quibusdam alias ipsam,
+            doloremque reprehenderit saepe fugit incidunt eaque nihil id
+            praesentium nesciunt! Ipsam, excepturi. Facilis, ea eos pariatur
+            adipisci iure fugit saepe. Facilis in quia nostrum. Recusandae
+            aperiam voluptate commodi nobis in qui consectetur exercitationem
+            cupiditate hic quo explicabo quam dolorem dolor iste inventore ipsum
+            voluptas velit aliquid, vitae tenetur facere doloribus laboriosam.
+            Quod, consequuntur expedita. Vitae dolore illo voluptates animi
+            corrupti minima accusamus ipsam praesentium repudiandae? Commodi,
+            minima ut accusamus quis numquam nihil eveniet exercitationem dicta
+            dolorem, in beatae laborum at. Perferendis totam vero autem? Dicta
+            dolores iure eum, fugit laudantium fugiat expedita sapiente itaque
+            aspernatur! Nulla ducimus hic sapiente quaerat neque cupiditate
+            placeat voluptas alias, cumque incidunt deleniti perferendis
+            perspiciatis dignissimos amet earum eligendi? Nemo corrupti quidem
+            eos saepe? Inventore tempore assumenda amet ab, vel voluptatibus
+            ducimus! Dolore dolorem molestiae esse nobis error quam cupiditate
+            illum pariatur doloremque, iure, iusto ab enim similique cumque.
+            Necessitatibus assumenda voluptatem illum quibusdam doloribus,
+            perferendis explicabo cupiditate tenetur blanditiis exercitationem
+            recusandae veritatis mollitia id cum magni omnis placeat. Distinctio
+            provident sit, assumenda eligendi possimus laborum qui ipsam animi.
+            Quos iste ut, eum harum fugiat cupiditate quibusdam, officia iusto
+            quam quae porro ducimus sunt libero dolorum quod deserunt nisi qui
+            tempore consequuntur sed consectetur molestias, rem nesciunt animi.
+            Labore! Sapiente tempora tenetur cum odit repellat placeat. Unde nam
+            id officia, voluptatum error tempore ut magnam iure soluta ratione
+            illum temporibus velit eos vero officiis amet possimus nostrum sunt
+            aliquam. Quis est molestias libero quod culpa, officiis vero aliquam
+            labore reprehenderit saepe, id expedita! Cum, dolor facilis dicta
+            voluptatum eveniet earum necessitatibus, natus voluptate labore,
+            amet et. Vitae, ullam dolor. Doloremque voluptates dolor impedit
+            quos tempore alias nisi repudiandae repellendus dolorum, quisquam
+            commodi enim sit unde minima. Laborum possimus at error architecto,
+            obcaecati delectus officia maiores temporibus veniam asperiores
+            odio. Doloribus rem sunt suscipit est totam nisi ipsa sapiente,
+            sequi praesentium. Rerum, pariatur laudantium accusamus quidem animi
+            deserunt ut facilis! Molestiae eum laborum similique maxime quidem
+            illo, quo est temporibus! Placeat quia doloremque quaerat corrupti
+            totam, sequi vel culpa. Vel iusto odit nisi laudantium ad sed
+            dolorum, reiciendis deleniti, veritatis corporis officiis aperiam
+            hic mollitia. Numquam cupiditate modi corporis accusantium.
+            Doloribus inventore quaerat recusandae omnis quae eligendi non
+            dignissimos. Excepturi id veritatis odit labore illo eos
+            exercitationem doloribus soluta, asperiores iusto explicabo tempore
+            laborum quisquam atque in necessitatibus, ipsam accusantium.
+            Expedita enim vero dignissimos consectetur. Expedita earum provident
+            dicta. Maiores officia, hic veritatis voluptate, voluptates itaque
+            sint illo cumque nisi aut iste ea quis porro est laboriosam. Nemo,
+            dolorum tenetur. Consequuntur tempore mollitia corrupti aspernatur
+            quo eveniet aliquam hic. Non, labore consectetur iure amet debitis
+            beatae itaque, odio laboriosam architecto praesentium quos fugit
+            autem animi laudantium dolores explicabo, placeat aspernatur!
+            Molestiae voluptate, veritatis reprehenderit ipsam tempora doloribus
+            qui asperiores enim eum omnis quas alias deleniti voluptatum minima
+            voluptates aliquam officiis facere suscipit incidunt earum
+            reiciendis dolore hic! Sequi, unde cum. Consectetur magnam placeat
+            delectus, cumque vero, vitae mollitia debitis quam dicta rerum,
+            praesentium excepturi impedit? Corporis impedit natus delectus quam?
+            In velit eum laborum. Aspernatur vitae fugit velit ea deserunt.
+            Suscipit laboriosam animi cum facere! Culpa dolores, iste
+            consequatur modi reiciendis quaerat, doloremque perferendis aut
+            voluptas expedita corporis earum, error illum rem similique nobis
+            obcaecati. Esse explicabo autem quibusdam ipsum! Dicta quaerat
+            dolorum voluptatibus eligendi quia esse nihil temporibus? Quae quia
+            praesentium ipsam consectetur assumenda sapiente, sint incidunt id
+            explicabo voluptate, necessitatibus quo vero tempora, totam neque
+            exercitationem veniam? Quae.
           </p>
         </div>
       </div>
---
+++ components/layout/navigation/navlinks.ts
@@ -0,0 +1,38 @@
+// app/constants/navlinks.ts
+import { Home, UserCircle, Settings, Users, Trophy } from "lucide-react";
+
+export const navLinks = [
+  {
+    title: "Accueil",
+    href: "/",
+    icon: Home,
+  },
+  {
+    title: "Membres",
+    href: "/members",
+    icon: Users,
+  },
+  {
+    title: "Compétitions",
+    href: "/competitions",
+    icon: Trophy,
+  },
+  {
+    title: "Profil",
+    href: "/profile",
+    icon: UserCircle,
+  },
+  {
+    title: "Paramètres",
+    href: "/settings",
+    icon: Settings,
+  },
+];
+
+export const topNavName: Record<string, string> = {
+  "/": "Accueil",
+  "/profile": "Profil",
+  "/members": "Membres",
+  "/settings": "Paramètres",
+  "/competitions": "Compétitions",
+};
