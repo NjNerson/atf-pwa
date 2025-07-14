9359738a454e15d12367bb3de5e1317f1885bf22
Nj Nerson <nj.nerson@gmail.com>
2025-07-14 20:49:24 +0300

    init profile page with loading

---
+++ app/profile/page.tsx
@@ -0,0 +1,11 @@
+import PageLayout from "@/components/layout/PageLayout";
+
+const Profile = () => {
+  return (
+    <PageLayout>
+      <h1 className="text-2xl font-bold text-center">Profil Page</h1>
+    </PageLayout>
+  );
+};
+
+export default Profile;
