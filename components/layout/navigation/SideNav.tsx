bfd64ca2fc6965877d9f4373ba3b9bd2f991c8b8
Nj Nerson <nj.nerson@gmail.com>
2025-07-14 20:52:33 +0300

    feat : logout

--- components/layout/navigation/SideNav.tsx
+++ components/layout/navigation/SideNav.tsx
@@ -4,6 +4,12 @@ import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 import { navLinks } from "./navlinks";
+import { Loader2 } from "lucide-react";
+import { useTransition } from "react";
+import { logOutUserAction } from "@/action/user/user";
+import { LogOut } from "lucide-react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 export default function SideNav() {
   const pathname = usePathname();
@@ -22,7 +28,7 @@ export default function SideNav() {
             <Link
               key={link.href}
               href={link.href}
-              className={`flex items-center gap-3 px-3 py-2 rounded-md text-sm font-medium transition-colors ${
+              className={`flex  duration-100 items-center gap-3 px-3 py-2 rounded-md text-sm font-normal transition-colors ${
                 isActive
                   ? "bg-blue-600 text-white shadow"
                   : "text-gray-700 hover:bg-blue-100 hover:text-blue-700"
@@ -34,6 +40,9 @@ export default function SideNav() {
           );
         })}
       </nav>
+      <div className="p-2 px-4 w-full border-t border-gray-200 flex flex-col">
+        <LogOutButton />
+      </div>
     </div>
   );
 }
@@ -52,3 +61,35 @@ const Header = () => (
     </h1>
   </div>
 );
+
+const LogOutButton = ({ className = "" }: { className?: string }) => {
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    startTransition(async () => {
+      const { errorMessage } = await logOutUserAction();
+      if (errorMessage) {
+        toast.error(errorMessage);
+      } else {
+        toast.success("Vous êtes déconnecté !");
+        router.push("/login");
+      }
+    });
+  };
+
+  return (
+    <button
+      onClick={handleLogout}
+      className={`flex gap-2 px-3 items-center justify-center mt-auto text-sm  py-1.5 text-red-500  rounded-md cursor-pointer hover:bg-red-100 transition-colors ${className}`}
+    >
+      {isPending ? (
+        <Loader2 className="size-5 animate-spin" />
+      ) : (
+        <LogOut className="size-5" />
+      )}
+
+      <span>Déconnexion</span>
+    </button>
+  );
+};
