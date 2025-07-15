b0f5e6651fc5212cf04d59d3d62328c7cdc64bd6
Nj Nerson <nj.nerson@gmail.com>
2025-07-15 17:42:33 +0300

    create Dashboard banner

---
+++ components/DashboardBanner.tsx
@@ -0,0 +1,16 @@
+const DashboardBanner = () => {
+  return (
+    <div className="shadow-lg w-full p-4 bg-white rounded-md">
+      <h1 className=" text-3xl md:text-3xl font-medium text-blue-900 py-2">
+        Bienvenue sur Dashboard{" "}
+        <span className="  text-red-400">ATF - Management</span> 👋
+      </h1>
+      <p className="md:text-lg ">
+        Gardez le contrôle sur vos membres, suivez leurs performances en
+        compétition et pilotez la vie du club jusqu’au bout ! 🥋
+      </p>
+    </div>
+  );
+};
+
+export default DashboardBanner;
