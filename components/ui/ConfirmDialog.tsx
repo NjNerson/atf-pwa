05cb5e370da13f15f75db8d31e37648c28285862
Nj Nerson <nj.nerson@gmail.com>
2025-07-15 15:57:02 +0300

    update style for mobile

--- components/ui/ConfirmDialog.tsx
+++ components/ui/ConfirmDialog.tsx
@@ -18,21 +18,21 @@ const ConfirmDialog = ({
   return (
     show && (
       <div
-        className="absolute top-0 left-0 z-100 w-screen h-screen bg-blue-100/20  
-            flex justify-center items-center"
+        className="absolute top-0 left-0 z-100 w-screen h-screen md:bg-blue-100/20  
+            flex justify-center items-center bg-blue-100/60"
         onClick={onCancel}
       >
         <div
           onClick={(e) => e.stopPropagation()}
-          className="relative md:w-1/4 md:mb-48 bg-white shadow-2xl rounded-md
+          className="relative md:w-1/4 w-3/4 md:mb-48 bg-white shadow-2xl rounded-md
                 p-4 text-gray-800  -translate-y-1/4"
         >
-          <div className="font-bold px-1.5  text-gray-900 border-b border-b-gray-200 pb-1">
+          <div className="font-bold px-1.5 md:text-base text-sm text-gray-900 border-b border-b-gray-200 pb-1">
             <h1 className="w-full h-full">
               {title ? title : "Demande de Confirmation"}
             </h1>
           </div>
-          <div className="p-1.5 text-base">
+          <div className="p-1.5 md:text-base text-sm">
             <p>
               {message
                 ? message
