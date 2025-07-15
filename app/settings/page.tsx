414f2e2804cb827501d45df14c60b173a03759b6
Nj Nerson <nj.nerson@gmail.com>
2025-07-15 15:57:35 +0300

    clearing settings page

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -2,6 +2,7 @@
 
 import { logOutUserAction } from "@/action/user/user";
 import PageLayout from "@/components/layout/PageLayout";
+import LogOutMobile from "@/components/LogOutMobile";
 import GroupMenu from "@/components/ui/GroupMenu";
 import MenuItem from "@/components/ui/MenuItem";
 import {
@@ -18,21 +19,6 @@ import { useTransition } from "react";
 import { toast } from "sonner";
 
 const Settings = () => {
-  const [isPending, startTransition] = useTransition();
-  const router = useRouter();
-
-  const handleLogout = async () => {
-    startTransition(async () => {
-      const { errorMessage } = await logOutUserAction();
-      if (errorMessage) {
-        toast.error(errorMessage);
-      } else {
-        toast.success("Vous êtes déconnecté !");
-        router.push("/login");
-      }
-    });
-  };
-
   return (
     <PageLayout className="flex flex-col gap-2 md:pb-16">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
@@ -76,22 +62,7 @@ const Settings = () => {
         </GroupMenu>
       </div>
 
-      <div className="w-full p-1 bg-white rounded-md shadow-lg md:hidden">
-        <MenuItem
-          link="/settings/category"
-          title="Déconnexion"
-          icon={
-            isPending ? (
-              <Loader2 className="size-5 animate-spin text-red-600" />
-            ) : (
-              <LogOut className="text-red-600" />
-            )
-          }
-          className="text-red-600 "
-          isButton={true}
-          onClick={handleLogout}
-        />
-      </div>
+      <LogOutMobile />
     </PageLayout>
   );
 };
