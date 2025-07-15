e25d2a145a6e3c464eeaa248052e72f8243a8a81
Nj Nerson <nj.nerson@gmail.com>
2025-07-15 15:45:44 +0300

    logout button with dialog

---
+++ components/LogOutButton.tsx
@@ -0,0 +1,52 @@
+import { logOutUserAction } from "@/action/user/user";
+import { Loader2, LogOut } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useTransition, useState } from "react";
+import { toast } from "sonner";
+import ConfirmDialog from "./ui/ConfirmDialog";
+
+const LogOutButton = ({ className = "" }: { className?: string }) => {
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
+  const [showLogOutDialog, setShowLogOutDialog] = useState(false);
+
+  const handleLogout = async () => {
+    startTransition(async () => {
+      const { errorMessage } = await logOutUserAction();
+      if (errorMessage) {
+        toast.error(errorMessage);
+      } else {
+        toast.success("Vous êtes déconnecté !");
+        router.push("/login");
+      }
+    });
+  };
+
+  const logout = () => {
+    setShowLogOutDialog(true);
+  };
+
+  return (
+    <>
+      <ConfirmDialog
+        show={showLogOutDialog}
+        onConfirm={handleLogout}
+        onCancel={() => setShowLogOutDialog(false)}
+      />
+      <button
+        onClick={logout}
+        className={`flex gap-2 px-3 items-center justify-center mt-auto text-sm  py-1.5 text-red-500  rounded-md cursor-pointer hover:bg-red-100 transition-colors ${className}`}
+      >
+        {isPending ? (
+          <Loader2 className="size-5 animate-spin" />
+        ) : (
+          <LogOut className="size-5" />
+        )}
+
+        <span>Déconnexion</span>
+      </button>
+    </>
+  );
+};
+
+export default LogOutButton;
