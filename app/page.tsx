0178511f3883811ad76a610d94f5438486534c9f
Nj Nerson <nj.nerson@gmail.com>
2025-07-15 17:42:40 +0300

    add banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,11 @@
+import DashboardBanner from "@/components/DashboardBanner";
 import PageLayout from "@/components/layout/PageLayout";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <PageLayout>
-      <div className="relative w-full max-w-4xl">
-        <div className="relative z-10 p-8 bg-white bg-opacity-80">
-          <h1 className="text-4xl font-bold text-center text-gray-800 mb-6">
-            Bienvenue sur ATF
-          </h1>
-          <p className="text-lg text-gray-600 text-center">
-            Votre application de gestion de tâches et de suivi des activités.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti
-            incidunt inventore aperiam quasi expedita sequi atque non quos autem
-            in aliquid itaque accusamus unde esse repellat odit, necessitatibus
-            mollitia molestias. Voluptatum inventore eos, quibusdam alias ipsam,
-            doloremque reprehenderit saepe fugit incidunt eaque nihil id
-            praesentium nesciunt! Ipsam, excepturi. Facilis, ea eos pariatur
-            adipisci iure fugit saepe. Facilis in quia nostrum. Recusandae
-            aperiam voluptate commodi nobis in qui consectetur exercitationem
-            cupiditate hic quo explicabo quam dolorem dolor iste inventore ipsum
-            voluptas velit aliquid, vitae tenetur facere doloribus laboriosam.
-            Quod, consequuntur expedita. Vitae dolore illo voluptates animi
-            corrupti minima accusamus ipsam praesentium repudiandae? Commodi,
-            minima ut accusamus quis numquam nihil eveniet exercitationem dicta
-            dolorem, in beatae laborum at. Perferendis totam vero autem? Dicta
-            dolores iure eum, fugit laudantium fugiat expedita sapiente itaque
-            aspernatur! Nulla ducimus hic sapiente quaerat neque cupiditate
-            placeat voluptas alias, cumque incidunt deleniti perferendis
-            perspiciatis dignissimos amet earum eligendi? Nemo corrupti quidem
-            eos saepe? Inventore tempore assumenda amet ab, vel voluptatibus
-            ducimus! Dolore dolorem molestiae esse nobis error quam cupiditate
-            illum pariatur doloremque, iure, iusto ab enim similique cumque.
-            Necessitatibus assumenda voluptatem illum quibusdam doloribus,
-            perferendis explicabo cupiditate tenetur blanditiis exercitationem
-            recusandae veritatis mollitia id cum magni omnis placeat. Distinctio
-            provident sit, assumenda eligendi possimus laborum qui ipsam animi.
-            Quos iste ut, eum harum fugiat cupiditate quibusdam, officia iusto
-            quam quae porro ducimus sunt libero dolorum quod deserunt nisi qui
-            tempore consequuntur sed consectetur molestias, rem nesciunt animi.
-            Labore! Sapiente tempora tenetur cum odit repellat placeat. Unde nam
-            id officia, voluptatum error tempore ut magnam iure soluta ratione
-            illum temporibus velit eos vero officiis amet possimus nostrum sunt
-          </p>
-        </div>
-      </div>
+    <PageLayout className="flex flex-col">
+      <DashboardBanner />
     </PageLayout>
   );
 }
